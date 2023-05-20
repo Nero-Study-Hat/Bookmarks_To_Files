cf97535c3ad9e7f3a45de85683d89de76dc466cd
Nero-Study-Hat <85171551+Nero-Study-Hat@users.noreply.github.com>
2023-05-19 23:54:42 -0400

    Get parent progress in test file.

--- practice/small_test.py
+++ practice/small_test.py
@@ -14,14 +14,23 @@ def get_dt_folder_tags(dt_tags_container) -> list:
 
 def main():
     dt_folder_tags: list = get_dt_folder_tags(_dt_tags)
-    folder_contents = get_folder_contents(dt_folder_tags[1])
+    folder_parent = get_folder_parent(dt_folder_tags[1])
     test = dt_folder_tags[1].next_sibling
     print(test)
 
-def get_folder_contents(dt_folder_tag):
-    p_tag = dt_folder_tag.find("dl").find_next()
-    folder_contents: list = p_tag.find_all("dt")
-    folder_tags: list = get_dt_folder_tags(folder_contents)
-    return folder_tags
+def get_folder_parent(dt_folder_tag, folder_tags_list: list) -> str:
+    loop_status: bool = False
+    count: int = 0
+    parent_tag = dt_folder_tag
+    while loop_status == False:
+        parent_tag = parent_tag.parent
+        if folder_tags_list.__contains__(parent_tag):
+            loop_status = True
+        count += 1
+    if count > 2:
+        pass
+    else:
+        parent_folder_name: str = parent_tag.find_next().text
+        
 
 main()
